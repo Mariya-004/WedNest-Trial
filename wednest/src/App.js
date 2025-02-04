6c409581aff45be648c2da8791b30bb9132689fe
Alina George <alinagrg03@gmail.com>
2025-02-04 20:14:44 +0530

    Update App.js

--- wednest/src/App.js
+++ wednest/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WeddingLandingPage from "./WeddingLandingPage";
 import Login from "./Login";
-import Signup from "./Signup"; // Single sign-up page for both roles
+import Signup from "./Signup";
+import VendorDashboard from "./VendorDashboard"; // Import the new dashboard
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WeddingLandingPage />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup />} /> {/* Single Sign-Up Page */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/vendor-dashboard" element={<VendorDashboard />} /> {/* Vendor Dashboard Route */}
       </Routes>
     </Router>
   );
